df5b2975966c10cfb1a06aed18dc16d8498f39e3
Mario Tabora <Mario@Marios-MBP.fios-router.home>
2019-01-20 20:14:26 -0500

    Client and Reffered_by attempt

--- irisdb/clients/models.py
+++ irisdb/clients/models.py
@@ -4,3 +4,18 @@ from django.db import models
 
 from django.db import models
 from django.utils import timezone
+
+
+class Client(models.Model):
+    client_firstname = models.CharField(max_length=10)
+    client_lastname = models.CharField(max_length=10
+    client_fullname = CONCAT(client_firstname, ' ',client_lastname)
+    created_date = models.DateTimeField('date created')
+    def __str__(self):
+        return self.client_fullname
+
+class Referred_by(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    referred_by_opts = models.CharField(mad_length=10)
+    def __str__(self):
+        return self.referred_by_opts

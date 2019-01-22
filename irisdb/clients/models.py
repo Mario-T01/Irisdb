b381c304ab355d49840aa2d4c71db3c8884ab100
Mario Tabora <Mario@Marios-MBP.fios-router.home>
2019-01-22 07:47:58 -0500

    /clients almost running, need database set up?

--- irisdb/clients/models.py
+++ irisdb/clients/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import datetime
 # Create your models here.
 
 from django.db import models
@@ -7,15 +7,22 @@ from django.utils import timezone
 
 
 class Client(models.Model):
-    client_firstname = models.CharField(max_length=10)
-    client_lastname = models.CharField(max_length=10
-    client_fullname = CONCAT(client_firstname, ' ',client_lastname)
-    created_date = models.DateTimeField('date created')
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=15)
+    client_created_date = models.DateTimeField('date created')
     def __str__(self):
-        return self.client_fullname
+        return self.client_firstname
+    def was_created_recently(self):
+        return self.created_date >= timezone.now()
+        datetime.timedelta(days=1)
+    #def fullname(self):
+    #    client_fullname = CONCAT(client_firstname, ' ',client_lastname)
+        #created_date = models.DateTimeField('date created')
+    #    return self.client_fullname
+
 
 class Referred_by(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    referred_by_opts = models.CharField(mad_length=10)
+    referred_by_opts = models.CharField(max_length=10)
     def __str__(self):
         return self.referred_by_opts
--- irisdb/clients/views.py
+++ irisdb/clients/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.template import loader
+from .models import Client
+
+from django.views import generic
+
+
+class IndexView(generic.ListView):
+    tempalte_name = 'clients/index.html'
+    content_object_name = 'latest_client_list'
+
+    def get_queryset(self):
+        """Return the last five created clients."""
+
+        return Client.objects.order_by('-created_date')[:5]

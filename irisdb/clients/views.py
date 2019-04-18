a4fe90f546a0eea1d09ac6766a900e5b8f3136a4
stonewalls91 <46726386+stonewalls91@users.noreply.github.com>
2019-04-17 21:59:13 -0400

    client_list grabs all the clients in database , gets top 3 names from and is in acending  order
       # client_list grabs all the clients in database 
    	  context_object_name = 'client_
    clients/views.py list'
       # gets top 3 names from and is in acending  order
    	  def get_queryset(self):

--- irisdb/clients/views.py
+++ irisdb/clients/views.py
@@ -8,12 +8,10 @@ from .models import Client
 
 from django.views import generic
 
-
 class IndexView(generic.ListView):
-    tempalte_name = 'clients/index.html'
-    content_object_name = 'latest_client_list'
-
-    def get_queryset(self):
-        """Return the last five created clients."""
-
-        return Client.objects.order_by('-created_date')[:5]
+	  template_name = 'clients/index.html'
+    # client_list grabs all the clients in database 
+	  context_object_name = 'client_list'
+    # gets top 3 names from and is in acending  order
+	  def get_queryset(self):
+	  	 return Client.objects.order_by('created_date')[:3]
